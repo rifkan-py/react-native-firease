9b3e20efe50f70ae4565f04d0dd391e21d7de9f4
rifkan <mohamedrifkan00127@gmail.com>
2023-05-13 23:56:17 +0530

    removed unused function

--- App.js
+++ App.js
@@ -6,14 +6,6 @@ import Home from "./screens/Home";
 import Login from "./screens/Login";
 import Register from "./screens/Register";
 
-const getFonts = () =>
-  Font.loadAsync({
-    "roboto-light": require("./assets/fonts/Roboto-Light.ttf"),
-    "roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
-    "roboto-medium": require("./assets/fonts/Roboto-Medium.ttf"),
-    "roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
-  });
-
 export default function App() {
   return <Home />;
 }
