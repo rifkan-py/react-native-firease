160e8bf69c08a799a9bf4534088557295a454293
rifkan <mohamedrifkan00127@gmail.com>
2023-05-15 12:08:40 +0530

    improved the styles of todo

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,10 +1,14 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function Todo() {
   return (
     <View style={styles.container}>
       <Text style={styles.todoText}>Learn React Native</Text>
-      <Text>{Date.now()}</Text>
+      <Text style={styles.date}>{new Date(Date.now()).toDateString()}</Text>
+      <TouchableOpacity style={styles.trash}>
+        <AntDesign name="delete" size={24} color="black" />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,9 +19,24 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 5,
     elevation: 5,
+    position: "relative",
   },
   todoText: {
     fontWeight: "bold",
     fontSize: 20,
+    textTransform: "uppercase",
+    color: "#444",
+    marginBottom: 20,
+  },
+  date: {
+    position: "absolute",
+    right: 10,
+    bottom: 5,
+    color: "#444",
+  },
+  trash: {
+    position: "absolute",
+    right: 10,
+    top: 5,
   },
 });
